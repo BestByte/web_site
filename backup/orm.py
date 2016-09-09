2cfe9e246ef8a6b076b4107b4fe6d2fda6c9bcb3
飞 <ilvxna@hotmail.com>
2016-09-09 09:07:52 +0800

    orm ready

--- backup/orm.py
+++ backup/orm.py
@@ -55,3 +55,54 @@ def create_args_string(num):
     for n in range(num):
         L.append('?')
     return ','.join(L)
+class Field():
+    def __init__(self, name,column_type,primary_key,default):
+        self.name=name
+        self.column_type=column_type
+        self.primarary_key=primarary_key
+        self.default=default
+    def __str__(self):
+        return '<%s,%s:%s>'%(self.__class__.__name__,self.column_type,self.name)
+class FloatField(Field):
+    def __init__(self,name=None,primary_key=False,default=0):
+        super().__init__(name,'real',primary_key,default)
+class StringField(Field):
+    def __init__(self,name=None,primary_key=False,default=None,ddl='varchar(100)'):
+        super().__init__(name,ddl,primary_key,default)
+class IntegerField(Field):
+    def __init__(self,name=None,primary_key=False,default=0):
+        super().__init__(name,'bigint',priamry_key,default)
+class BooleanField(Field):
+    def __init__(self,name=None,primary_key=False,default=0):
+        super().__init__(name,'boolean',False,default)
+class TextField(Field):
+    def __init__(self,name=None,default=None):
+        super().__init__(name,'text',False,default)
+class ModelMetaClass(type):
+    def __new__(cls,name,bases,attrs):
+        if name=='Model':
+            return type.__new__(cls,name,bases,attrs)
+            tableName=attrs.get('__table__',name) or name
+            logging.info("found Model:%s (table:%s)"% (name,tableName))
+            mappings =dict()
+            fields=[]
+            primarykey=None
+            for k,v in attrs.items():
+                if isinstance(v, Field):
+                    logging.info('found mapping:%s==>%s'%(k,v))
+                    mapping=k
+                    if v.primary_key:
+                        # found main key()
+                        if primary_key:
+                            raise AttributeError("Duplicate primary key for field:%s"% k )
+                        primarykey =k
+                    else:
+                        fields.append(k)
+            if not primarykey:
+                raise AttributeError("primary key not found")
+            for k in mapping.keys():
+                attrs.pop(k)
+            escaped_fields=list(map(lambda f:'%s'% f,fields))
+            attrs['__mapping__']=mappings
+                
+        
