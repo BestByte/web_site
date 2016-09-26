acc882cd94650d7243aa53fd9fe6a035769d3bea
飞 <ilvxna@hotmail.com>
2016-09-26 21:52:05 +0800

    add get

--- backup/coroweb.py
+++ backup/coroweb.py
@@ -1,3 +1,14 @@
 import asyncio,os,inspect,logging,functools
 from urlib import parse
 from aiohttp import web
+from apis import APIError
+def get(path):
+    def decorotator(func):
+        @functools.wraps
+        def wrapper(*args,**kw):
+            return func(*args,**kw)
+        wrapper.__method='GET'
+        wrapper.__route__=path
+        return wrapper
+    return decorator
+    
